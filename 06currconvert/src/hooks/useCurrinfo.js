298d95c302d9d937ce165a5ee0f47b1668948eea
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-05-17 22:36:40 +0530

    07router

--- 06currconvert/src/hooks/useCurrinfo.js
+++ 06currconvert/src/hooks/useCurrinfo.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 //hooks are like function
 function useCurrInfo(currency) {
+   const [data, setData] = useState({})
   //useEffect is a hook that allows you to perform side effects in function components
   useEffect(() => {
     // Your side effect logic here
-    const [data, setData] = useState({});
-    fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
-    )
+   
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
-    console.table(data);
+    //console.table(data);
   }, [currency]);
   console.log(data);
   return data;
