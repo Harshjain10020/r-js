01654c5dddd64ca14c756efbdba5f71cbe534ae6
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-05-22 23:46:30 +0530

    bug-fixed

--- 10todocontext/src/App.jsx
+++ 10todocontext/src/App.jsx
@@ -1,38 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import { TodoProvider } from "./Context";
-import { preconnect } from "react-dom";
+import TodoForm from "./Component/TodoForm";
+import TodoItem from "./Component/Todoitem";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const addTodo = (todo)=>{
-    setTodos((prev)=>{
-      [{id: Date.now(), ...todo},...prev]
-    })
+  const addTodo = (todo) => {
+  setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+};
+
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+    //     prev.map((eachVal)=>{
+    //       if(eachVal.id===id){
+    // todo
+    //       }else{
+    //         eachVal
+    //       }
+    //     })
+  };
+
+  const delTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+  setTodos((prev) =>
+    prev.map((todo) =>
+      todo.id === id ? { ...todo, checked: !todo.checked } : todo
+    )
+  );
+};
+
+
+  useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem("todos"));
+  if (storedTodos && Array.isArray(storedTodos)) {
+    setTodos(storedTodos);
   }
-
-  const updateTodo = (id, todo)=>
-    {
-    setTodos((prev)=> prev.map((prevTodo)=>(prevTodo.id === id ? todo : prevTodo )))
-//     prev.map((eachVal)=>{
-//       if(eachVal.id===id){
-// todo
-//       }else{
-//         eachVal
-//       }
-//     })
-  }
+}, []);
 
 
-  const delTodo = (id)=>{
-    setTodos((pre)=> prev.filter((todo)=> todo.id !== id))
-  }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-
-  const toggleTodo = (id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=> prevTodo === id ? {...prevTodo,checked : !prevTodo.checked}: prevTodo))
-  }
   return (
     //value  is like destructureing context
     <TodoProvider value={{ todos, delTodo, updateTodo, addTodo, toggleTodo }}>
@@ -41,9 +58,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
